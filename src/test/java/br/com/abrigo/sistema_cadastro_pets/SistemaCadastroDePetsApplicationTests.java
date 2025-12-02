101cf1deae4b2a20391cd66236973c106aad6ff1
Karine <contato.karinecostap@gmail.com>
2025-12-02 15:40:56 -0300

    feat(setup): configuracao inicial da api rest, postgres e docker

---
+++ src/test/java/br/com/abrigo/sistema_cadastro_pets/SistemaCadastroDePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abrigo.sistema_cadastro_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroDePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
