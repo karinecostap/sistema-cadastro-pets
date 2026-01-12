3040649aae290da2ac844c4b24c2f749c834814a
Karine <contato.karinecostap@gmail.com>
2026-01-12 16:04:30 -0300

    feat: Implementa  a funcao de cadastrar e excluir adotante

---
+++ src/main/java/br/com/abrigo/sistema_cadastro_pets/controller/AdotanteController.java
@@ -0,0 +1,29 @@
+package br.com.abrigo.sistema_cadastro_pets.controller;
+import br.com.abrigo.sistema_cadastro_pets.dto.AdotanteCadastroDTO;
+import br.com.abrigo.sistema_cadastro_pets.service.AdotanteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/adotantes")
+public class AdotanteController {
+    @Autowired
+    private AdotanteService adotanteService;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody AdotanteCadastroDTO adotanteCadastroDTO) {
+        try {
+            var result = this.adotanteService.cadastrarAdotante(adotanteCadastroDTO);
+            return ResponseEntity.created(null).body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>delete(@PathVariable Integer id){
+        adotanteService.deletarAdotante(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/br/com/abrigo/sistema_cadastro_pets/repository/EnderecoRepository.java
+++ src/main/java/br/com/abrigo/sistema_cadastro_pets/repository/EnderecoRepository.java
@@ -2,6 +2,8 @@ package br.com.abrigo.sistema_cadastro_pets.repository;
 
 import br.com.abrigo.sistema_cadastro_pets.model.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface EnderecoRepository extends JpaRepository <Endereco, Integer> {
+    Optional<Endereco> findByCepAndNumero(String cep, String numero);
 }
